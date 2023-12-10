5e6c5d52d33c092c452f3bc7442e0fd9c22540d9
faelern <mico.mg52@gmail.com>
2023-12-10 22:39:23 +0100

    day 10 part 2 try 2

--- day10/part2.py
+++ day10/part2.py
@@ -4,6 +4,8 @@ directions = {'|': 'NS', '-': 'EW', 'L': 'NE', 'J': 'NW', '7': 'SW', 'F': 'SE'}
 
 dir_change = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
 
+bends = ['L', 'J', '7', 'F', 'S']
+
 start_pos = []
 with open('input.txt', 'r') as file:
     for line in file:
@@ -13,10 +15,10 @@ with open('input.txt', 'r') as file:
 loop = []
 steps = 1
 
+y = 1
+x = 2
 
-y = 31
-x = 112
-
+start_pos = [y, x]
 
 char = lines[y][x]
 came_from = 'W'
@@ -39,23 +41,37 @@ while char != 'S':
         going_to = directions[char].replace(came_from, '')
     steps += 1
 
-print(steps/2)
+print('steps', steps/2)
 
-inner = 0
-outer = 0
+loop.append(start_pos)
+# inner = 0
+# outer = 0
+#
+# for y, line in enumerate(lines):
+#     intersect = 0
+#     for x, char in enumerate(line):
+#         if [y, x] in loop:
+#             intersect += 1
+#         else:
+#             if intersect % 2 == 0:
+#                 outer += 1
+#             else:
+#                 inner += 1
 
-for y, line in enumerate(lines):
-    intersect = 0
-    for x, char in enumerate(line):
-        if [y, x] in loop:
-            intersect += 1
-        else:
-            if intersect % 2 == 0:
-                outer += 1
-            else:
-                inner += 1
+vertices = [point for point in loop if lines[point[0]][point[1]] in bends]
 
-print(inner)
+# vertices.append(vertices[0])
 
-# poziome rury psuja
+area = 0
+for i in range(len(vertices) - 1):
+    area += vertices[i][1] * vertices[i + 1][0] - vertices[i][0] * vertices[i + 1][1]
+    print('foo')
+area += vertices[-1][1] * vertices[0][0] - vertices[-1][0] * vertices[0][1]
 
+print('loop', len(loop))
+area = abs(area) / 2
+print('area', area)
+
+
+inner = area - len(loop)/2 + 1
+print('inner', inner)

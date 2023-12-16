10868d4b3cd1c32e0bcc4f0e23cf6d10ee8a7916
faelern <mico.mg52@gmail.com>
2023-12-16 15:24:49 +0100

    day 16 part 2

--- day16/part2.py
+++ day16/part2.py
@@ -3,13 +3,9 @@ with open('/home/faelern/PycharmProjects/advent_of_code/day16/input.txt', 'r') a
     for line in file:
         data.append(line[:-1])
 
-beams = []
 grid = [[[] for y in range(len(data[0]))] for x in range(len(data))]
 xmax = len(data[0]) - 1
 ymax = len(data) - 1
-energized = set()
-history = set()
-values = []
 
 change_dir = {'/': {'n': 'e', 'e': 'n', 's': 'w', 'w': 's'},
               '\\': {'n': 'w', 'e': 's', 's': 'e', 'w': 'n'},
@@ -19,19 +15,22 @@ change_dir = {'/': {'n': 'e', 'e': 'n', 's': 'w', 'w': 's'},
 start_pos_list = []
 for y in range(ymax + 1):
     start_pos_list.append([y, 0, 'e'])
-    start_pos_list.append([y, xmax + 1, 'w'])
+    start_pos_list.append([y, xmax, 'w'])
 
 for x in range(xmax + 1):
     start_pos_list.append([0, x, 's'])
-    start_pos_list.append([ymax + 1, x, 'n'])
+    start_pos_list.append([ymax, x, 'n'])
 
+max_value = 0
 
 for start_pos in start_pos_list:
     beams = [start_pos]
+    energized = set()
+    energized.add((start_pos[0], start_pos[1]))
+    history = set()
 
     while beams:
         for i, beam in enumerate(beams):
-            # print(i, beam)
             y, x, d = beam
             temp = (y, x, d)
             if temp in history:
@@ -63,7 +62,8 @@ for start_pos in start_pos_list:
 
             beams[i] = [y, x, d]
             history.add(temp)
+    value = len(energized)
+    if value > max_value:
+        max_value = value
 
-        values.append(len(energized) + 1)
-
-print(max(values))
+print(max_value)
